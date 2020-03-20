aa53934c8b1baf7ce31e36668e2f114042c773d4
Will Harris <will.t.harris@protonmail.com>
2020-03-19 23:03:15 -0700

    feat: add theme ui

---
+++ packages/www/wrap-root-element.js
@@ -0,0 +1,7 @@
+const React = require("react");
+const { ThemeProvider } = require("theme-ui");
+const { deep } = require("@theme-ui/presets");
+
+module.exports = ({ element }) => (
+	<ThemeProvider theme={deep}>{element}</ThemeProvider>
+);
