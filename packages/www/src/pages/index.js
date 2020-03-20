2aada66869ed198c0126d949d2c4e9bf879ff53f
Will Harris <will.t.harris@protonmail.com>
2020-03-19 23:10:07 -0700

    feat: mock basic home page

--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { Container, Heading, Button, Flex } from "theme-ui";
 
 export default props => (
-	<div>
-		<h1>The Site</h1>
-	</div>
+	<Container>
+		<Flex sx={{ flexDirection: "column", padding: 3 }}>
+			<Heading as="h1">Get Stuff Done</Heading>
+			<Button
+				sx={{ marginTop: 2 }}
+				onClick={() => {
+					alert("clicked!");
+				}}
+			>
+				Log In
+			</Button>
+		</Flex>
+	</Container>
 );
