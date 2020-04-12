07c03916c50766d6b91d869a286145cfe189562e
Will Harris <will.t.harris@protonmail.com>
2020-04-11 18:03:51 -0700

    add basic graphql server

---
+++ functions/graphql/graphql.js
@@ -0,0 +1,28 @@
+const { ApolloServer, gql } = require("apollo-server-lambda");
+
+// Construct schema
+const typeDefs = gql`
+	type Query {
+		hello: String
+	}
+`;
+
+// Provide resolver functions for schema fields
+const resolvers = {
+	Query: {
+		hello: () => "Hello world!",
+	},
+};
+
+const server = new ApolloServer({
+	typeDefs,
+	resolvers,
+
+	// By default, the GraphQL Playground interface and GraphQL introspection
+	// is disabled in "production" (i.e. when `process.env.NODE_ENV` is `production`).
+	//
+	// If you'd like to have GraphQL Playground and introspection enabled in production,
+	// the `playground` and `introspection` options must be set explicitly to `true`.
+	playground: true,
+	introspection: true,
+});
