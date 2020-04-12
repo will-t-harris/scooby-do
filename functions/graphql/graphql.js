7b26175f2f2cd77baabcfefe30fdd92b509b4f45
Will Harris <will.t.harris@protonmail.com>
2020-04-12 12:26:35 -0700

    add mutation type and resolvers

--- functions/graphql/graphql.js
+++ functions/graphql/graphql.js
@@ -10,15 +10,32 @@ const typeDefs = gql`
 		text: String!
 		done: Boolean!
 	}
+	type Mutation {
+		addTodo(text: String!): Todo
+		updateTodoDone(id: ID!): Todo
+	}
 `;
 
 const todos = {};
+let todoIndex = 0;
 
 // Provide resolver functions for schema fields
 const resolvers = {
 	Query: {
 		todos: () => {
 			return Object.values(todos);
+		},
+	},
+	Mutation: {
+		addTodo: (_, { text }) => {
+			todoIndex++;
+			const id = `key-${todoIndex}`;
+			todos[id] = { id, text, done: false };
+			return todos[id];
+		},
+		updateTodoDone: (_, { id }) => {
+			todos[id].done = true;
+			return todos[id];
 		},
 	},
 };
