374661b17676e599ce14e448b7217a60313d16cd
Will Harris <will.t.harris@protonmail.com>
2020-04-12 12:40:24 -0700

    provide ApolloClient to app

--- packages/www/wrap-root-element.js
+++ packages/www/wrap-root-element.js
@@ -1,12 +1,28 @@
 const React = require("react");
 const { ThemeProvider } = require("theme-ui");
 const { deep } = require("@theme-ui/presets");
+const {
+	ApolloProvider,
+	ApolloClient,
+	HttpLink,
+	InMemoryCache,
+} = require("@apollo/client");
+
 const { Provider } = require("./identity-context");
 
 const tokens = { ...deep, sizes: { container: 1024 } };
 
+const client = new ApolloClient({
+	cache: new InMemoryCache(),
+	link: new HttpLink({
+		uri: "https://scooby-do.netlify.com/.netlify/functions/graphql",
+	}),
+});
+
 module.exports = ({ element }) => (
 	<Provider>
-		<ThemeProvider theme={tokens}>{element}</ThemeProvider>
+		<ApolloProvider client={client}>
+			<ThemeProvider theme={tokens}>{element}</ThemeProvider>
+		</ApolloProvider>
 	</Provider>
 );
